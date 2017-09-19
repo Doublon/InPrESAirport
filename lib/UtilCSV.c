6c6c300123fe71db51ad824ac879cfb0da67269e
Zeydax <phil3001@hotmail.com>
2017-09-19 16:02:52 +0200

    v2

--- lib/UtilCSV.c
+++ lib/UtilCSV.c
@@ -5,22 +5,31 @@ FILE* OuvrirCSV(char * NomFichier, char* Mode) {
 }
 
 int FermerCSV(FILE* fp) {
-    return fclose(FileDes);
+    return fclose(fp);
 }
 
-char * LireCSV (FILE* fp, char Delimiteur) {
+char * LireCSV (char* NomFichier, char Delimiteur) {
     int i = 0;
     char buff[MAXSTRING];
     char tab[255][2];
+    FILE *fp;
 
-    while (!feof(fp))
+    fp = fopen (NomFichier, "rb");
+    if (fp == NULL)
     {
-        fgets(buff, 100, fp);
+        printf("Erreur rencontree lors de l'ouverture du fichier !\n");
+    }
+    else
+    {
+        while (!feof(fp))
+        {
+            fgets(buff, 100, fp);
 
-        tab[i][0] = strtok(buff, Delimiteur);
-        tab[i][1] = strtok(NULL, EOL);
+            tab[i][0] = strtok(buff, Delimiteur);
+            tab[i][1] = strtok(NULL, EOL);
 
-        i++;
+            i++;
+        }
     }
     
     return tab;
